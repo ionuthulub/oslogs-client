b8a9aa27dfcc10d4495416ccbaec32e1f761506c
Ionut Hulub <ionut.hulub@gmail.com>
2015-06-26 21:39:46 +0300

    monitor files

--- oslogs-client.py
+++ oslogs-client.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import json
-import sys
 import os
 import traceback
 
@@ -8,24 +7,45 @@ import pika
 
 
 HOSTNAME = os.uname()[1]
+QUEUE = ROUTING_KEY = 'oslogs'
+
+CONNECTION = pika.BlockingConnection(pika.ConnectionParameters(
+    host='192.168.37.100'))
+CHANNEL = CONNECTION.channel()
+CHANNEL.queue_declare(queue=QUEUE)
 
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='192.168.37.100'))
-    channel = connection.channel()
+    with open('logs.txt', 'r') as fin:
+        log_paths = [l.strip() for l in fin.readlines()]
+    logs = []
+    for log_path in log_paths:
+        try:
+            logs.append(open(log_path, 'r'))
+            logs[-1].seek(0, 2)
+        except Exception, err:
+            print 'Failed to open "%s"; error "%s"' % (log_path, err)
+    if not logs:
+        print 'No logs found. Exiting...'
+        return
+    while True:
+        try:
+            for log in logs:
+                line = log.readline()
+                if line:
+                    log_path = log.name
+                    send_update(log_path, line)
+        except KeyboardInterrupt:
+            print 'Exiting...'
+            CONNECTION.close()
+            return
 
-    channel.queue_declare(queue='oslogs')
 
-    msg = {'host': HOSTNAME, 'path': sys.argv[1], 'msg': sys.argv[2]}
-    msg = json.dumps(msg)
-
-    channel.basic_publish(exchange='',
-                          routing_key='oslogs',
-                          body=msg)
-    print " [x] Sent msg %s" % msg
-    connection.close()
-
+def send_update(path, msg):
+    update = {'host': HOSTNAME, 'path': path, 'msg': msg}
+    update = json.dumps(update)
+    CHANNEL.basic_publish(exchange='', routing_key=ROUTING_KEY, body=update)
+    print 'Sent "%s"' % update
 
 if __name__ == '__main__':
     try:
